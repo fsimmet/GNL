0e107d397ea8ffaf0a208ba5d40cad488cfc844e
Floriane SIMMET <fsimmet@e2r5p10.42.fr>
2016-04-14 20:28:08 +0200

    fd lst

--- get_next_line.c
+++ get_next_line.c
@@ -6,17 +6,16 @@
 /*   By: fsimmet <fsimmet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/27 12:43:24 by fsimmet           #+#    #+#             */
-/*   Updated: 2016/04/13 20:29:33 by fsimmet          ###   ########.fr       */
+/*   Updated: 2016/04/14 20:26:40 by fsimmet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include "string.h"
 
 static int	get_buf(int const fd, char **lines, char **remain)
 {
 	ssize_t		red;
-	char		buf[BUFF_SIZE];
+	char		buf[BUFF_SIZE + 1];
 	int			i;
 
 	while ((red = read(fd, buf, BUFF_SIZE)))
@@ -41,16 +40,38 @@ static int	get_buf(int const fd, char **lines, char **remain)
 	return ((*lines != NULL) ? 1 : red);
 }
 
+static t_fd		*get_current_fd(int const fd, t_list **fds)
+{
+	t_list	*tmp;
+	t_fd	content;
+
+	tmp = *fds;
+	while (tmp != NULL)
+	{
+		if (((t_fd *)tmp->content)->fd == fd)
+		{
+			return (tmp->content);
+		}
+		tmp = tmp->next;
+	}
+	content.fd = fd;
+	content.res = NULL;
+	ft_lstadd(fds, ft_lstnew(content, sizeof(content)));
+	return ((*fds)->content);
+}
+
 int			get_next_line(int const fd, char **line)
 {
-	size_t		i;
-	static char	*remain[256];
-	char		*tmp;
+	size_t			i;
+	static t_list	*fds = NULL;
+	char			*tmp;
+	t_fd			*current;
 
-	if (fd < 0 || fd > 255 || line == NULL || BUFF_SIZE <= 0)
+	if (line == NULL || BUFF_SIZE <= 0)
 		return (-1);
 	*line = NULL;
-	if (remain[fd] && remain[fd][0])
+	current = get_current_fd(fd, fds);
+	if (remain[fd] && remain[][0])
 	{
 		i = 0;
 		while (remain[fd][i] && remain[fd][i] != '\n')
--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,22 @@
 /*   By: fsimmet <fsimmet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/27 12:43:38 by fsimmet           #+#    #+#             */
-/*   Updated: 2016/04/14 11:06:40 by fsimmet          ###   ########.fr       */
+/*   Updated: 2016/04/14 20:26:43 by fsimmet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include "libft/includes/libft.h"
+# include "libft.h"
+# define BUFF_SIZE 8
 # include <fcntl.h>
-# define BUFF_SIZE 100
+
+typedef struct	s_fd
+{
+	int			fd;
+	char		*res;
+}				t_fd;
 
 int				get_next_line(int const fd, char **line);
 
