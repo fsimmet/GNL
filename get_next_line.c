228448bc875ecbe144f3727e93ffae44a5bb9e5b
Floriane SIMMET <fsimmet@e2r5p10.42.fr>
2016-04-14 11:06:56 +0200

    GNL ok

--- get_next_line.c
+++ get_next_line.c
@@ -6,19 +6,18 @@
 /*   By: fsimmet <fsimmet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/27 12:43:24 by fsimmet           #+#    #+#             */
-/*   Updated: 2016/02/16 12:17:04 by fsimmet          ###   ########.fr       */
+/*   Updated: 2016/04/13 20:29:33 by fsimmet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include "string.h"
 
-static int	get_buf(int const fd, char **lines, char **res)
+static int	get_buf(int const fd, char **lines, char **remain)
 {
 	ssize_t		red;
 	char		buf[BUFF_SIZE];
 	int			i;
-	char		*tmp;
 
 	while ((red = read(fd, buf, BUFF_SIZE)))
 	{
@@ -31,38 +30,42 @@ static int	get_buf(int const fd, char **lines, char **res)
 		{
 			i = 0;
 			while (buf[i] != '\0' && buf[i] != '\n')
-				*lines = ft_strfjoin(*lines, ft_chartostar(buf[i++]), 3);
-			tmp = ft_strchr(buf, '\n');
-			res[fd] = ft_strdup(&tmp[1]);
+			{
+				*lines = ft_strfjoin(*lines, ft_chartostar(buf[i]), 3);
+				i++;
+			}
+			*remain = ft_strdup(&(ft_strchr(buf, '\n'))[1]);
 			return (1);
 		}
 	}
-	return ((*lines) ? 1 : red);
+	return ((*lines != NULL) ? 1 : red);
 }
 
 int			get_next_line(int const fd, char **line)
 {
 	size_t		i;
-	static char	*res[FDMAX];
+	static char	*remain[256];
 	char		*tmp;
 
-	if (fd < 0 || fd > FDMAX || line == NULL || BUFF_SIZE <= 0)
+	if (fd < 0 || fd > 255 || line == NULL || BUFF_SIZE <= 0)
 		return (-1);
-	if (*line)
-		*line = NULL;
-	if (res[fd] && res[fd][0])
+	*line = NULL;
+	if (remain[fd] && remain[fd][0])
 	{
 		i = 0;
-		while (res[fd][i] && res[fd][i] != '\n')
-			*line = ft_strfjoin(*line, ft_chartostar(res[fd][i++]), 3);
-		if ((tmp = ft_strchr(&res[fd][i], '\n')))
+		while (remain[fd][i] && remain[fd][i] != '\n')
 		{
-			tmp = ft_strchr(&res[fd][i], '\n');
-			res[fd] = ft_strdup(&tmp[1]);
+			*line = ft_strfjoin(*line, ft_chartostar(remain[fd][i]), 3);
+			i++;
+		}
+		if (i == 0)
+			*line = ft_strnew(0);
+		if ((tmp = ft_strchr(&remain[fd][i], '\n')))
+		{
+			remain[fd] = ft_strcpy(remain[fd], &tmp[1]);
 			return (1);
 		}
-		else
-			res[fd] = NULL;
+		remain[fd] = NULL;
 	}
-	return (get_buf(fd, line, res));
+	return (get_buf(fd, line, &remain[fd]));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: fsimmet <fsimmet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/27 12:43:38 by fsimmet           #+#    #+#             */
-/*   Updated: 2016/03/20 20:07:23 by fsimmet          ###   ########.fr       */
+/*   Updated: 2016/04/14 11:06:40 by fsimmet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,7 @@
 
 # include "libft/includes/libft.h"
 # include <fcntl.h>
-# define FDMAX 256
-# define BUFF_SIZE 1
+# define BUFF_SIZE 100
 
 int				get_next_line(int const fd, char **line);
 
